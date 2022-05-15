a40ba97cd25ddaa7f1bc72e9881a423bf275e827
Vinicius Lopes <48124913+VLopes4@users.noreply.github.com>
2022-05-15 02:12:24 -0300

    ReactJS: Capítulo I

---
+++ React/Capitulo I/desafio-componentizando-a-aplicacao/src/App.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+import './styles/global.scss';
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar selectedGenreId={selectedGenreId} setSelectedGenreId={setSelectedGenreId}/>
+      <Content selectedGenreId={selectedGenreId} setSelectedGenreId={setSelectedGenreId}/>
+    </div>
+  )
+}
