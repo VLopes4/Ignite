98689990d11e8ed566392c1a3b7239cd4f0eb9f3
vinicius <vinnicius4@hotmail.com>
2023-03-23 00:36:55 -0300

    projeto 03: estrutura do projeto, integração com prismas orm, caso de uso e design patterns e design patterns & testes.

---
+++ Node/03-api-solid/src/http/routes.ts
@@ -0,0 +1,8 @@
+import { FastifyInstance } from "fastify";
+import { Register } from "./controllers/register";
+
+export async function appRoutes(app: FastifyInstance) {
+  const register = new Register()
+
+  app.post('/users', register.create)
+}
