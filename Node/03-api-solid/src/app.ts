a7bd24c70d5a9ddf1c7f72914367d1933f1c897c
vinicius <vinnicius4@hotmail.com>
2023-03-29 23:31:04 -0300

    controllers & testes E2E

--- Node/03-api-solid/src/app.ts
+++ Node/03-api-solid/src/app.ts
@@ -1,11 +1,19 @@
+import { fastifyJwt } from "@fastify/jwt";
 import fastify from "fastify";
 import { ZodError } from "zod";
 import { env } from "./env";
-import { appRoutes } from "./http/routes";
+import { checkInsRoutes } from "./http/controllers/check-ins/routes";
+import { gymsRoutes } from "./http/controllers/gyms/routes";
+import { usersRoutes } from "./http/controllers/users/routes";
 
 export const app = fastify();
 
-app.register(appRoutes)
+app.register(fastifyJwt, { secret: env.JWT_SECRET })
+
+app.register(usersRoutes)
+app.register(checkInsRoutes)
+app.register(gymsRoutes)
+
 app.setErrorHandler((error, request, reply) => {
   if (error instanceof ZodError) {
     return reply
