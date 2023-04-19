149169754ce7439225ca85e2a29a91dbb6a6aa36
vinicius <vinnicius4@hotmail.com>
2023-04-18 23:58:12 -0300

    Produto & Checkout

--- React/2022/04-ignite-shop/src/pages/index.tsx
+++ React/2022/04-ignite-shop/src/pages/index.tsx
@@ -1,16 +1,26 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import Head from 'next/head';
 
 import { useKeenSlider } from 'keen-slider/react'
 
-import camiseta1 from '../assets/camisetas/1.png';
-import camiseta2 from '../assets/camisetas/2.png';
-import camiseta3 from '../assets/camisetas/3.png';
-import camiseta4 from '../assets/camisetas/4.png';
-
 import { HomeContainer, Product } from '@/styles/pages/home';
 import 'keen-slider/keen-slider.min.css'
+import { stripe } from '@/lib/stripe';
+import { GetStaticProps } from 'next';
+import Stripe from 'stripe';
 
-export default function Home() {
+
+interface HomeProps {
+  products: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3, 
@@ -19,42 +29,51 @@ export default function Home() {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className='keen-slider'>
-      <Product className='keen-slider__slide'>
-        <Image src={camiseta1} alt='' width={520} height={480}/>
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+      <HomeContainer ref={sliderRef} className='keen-slider'>
+        {products.map(product => (
+          <Link key={product.id} href={`/product/${product.id}`} prefetch={false}>
+            <Product className='keen-slider__slide'>
+              <Image src={product.imageUrl} alt='' width={520} height={480}/>
 
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className='keen-slider__slide'>
-        <Image src={camiseta2} alt='' width={520} height={480}/>
-
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className='keen-slider__slide'>
-        <Image src={camiseta3} alt='' width={520} height={480}/>
-
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className='keen-slider__slide'>
-        <Image src={camiseta4} alt='' width={520} height={480}/>
-
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-    </HomeContainer>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
+        ))}
+      </HomeContainer>
+    </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount! / 100), // Preço do Stripe retorna em centavos
+    }
+  })
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60 * 60 * 2 // 2h
+  }
 }
--- React/2022/04-ignite-shop/src/styles/pages/home.ts
+++ React/2022/04-ignite-shop/src/styles/pages/home.ts
@@ -43,7 +43,8 @@ export const Product = styled('div', {
 
 
     strong: {
-      fontSize: '$lg'
+      fontSize: '$lg',
+      color: '$gray100'
     },
 
     span: {
