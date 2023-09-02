d2d2da078b278c1dba7529eb75b76232104c7706
vinicius <vinnicius4@hotmail.com>
2023-09-02 00:45:45 -0300

    Fundamentos do Nest

---
+++ Node/05-nest-clean/src/controllers/fetch-recent-question.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Query, UseGuards } from "@nestjs/common";
+import { z } from 'zod';
+import { PrismaService } from "@/prisma/prisma.service";
+import { ZodValidationPipe } from "@/pipes/zod-validation.pipe";
+import { JwtAuthGuard } from "@/auth/jwt-auth.guard";
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1)
+)
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class FetchRecentQuestionsController {
+  constructor(private prisma: PrismaService) {}
+  
+  @Get()
+  async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
+    const perPage = 20
+
+    const questions = await this.prisma.question.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createdAt: 'desc'
+      }
+    })
+
+    return questions
+  }
+}
